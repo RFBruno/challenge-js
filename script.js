3e6a1245b6f40badf74cabc235de2b7cb2be6b2d
RFBruno <bruno_rafael_08@hotmail.com>
2021-05-18 21:20:06 -0300

    script updated

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-let setInterval = 
+let min = 0;
+let sec = 0;
+let mili = 0;
+let time;
+
+let callback = () => {
+    sec++;
+
+    if(sec == 60){
+        min++;
+        sec = 0;
+    }
+
+    let time = `${min < 10 ? '0'+min : min}:${sec < 10 ? '0'+sec : sec}.${mili < 10 ? '0'+mili : mili}`;
+    document.querySelector('#tempo span').innerHTML = time;
+}
+
+function start(){
+   time = setInterval(callback,1000);
+}
+
+function reset(){
+    sec = 0;
+    min = 0;
+    mili = 0;
+    let formatTime = `${min < 10 ? '0'+min : min}:${sec < 10 ? '0'+sec : sec}`;
+    document.querySelector('#tempo span').innerHTML = formatTime;
+    clearInterval(time);
+}
+
+
